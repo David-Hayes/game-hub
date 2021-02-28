bae4494cb8b550bb64ddadf2c7e6ae94a1bb2cef
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-02-28 23:10:59 +0000

    Building out game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,3 +1,82 @@
-export const Game = () => {
-  return <></>
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { EP_GAME } from '../config/Endpoints'
+import { Loader } from '../components/Loader'
+import { Heading1 } from '../components/Headings'
+import { Wrapper } from '../components/Layout'
+
+export const Game = (props) => {
+  const [game, setGame] = useState(null)
+
+  useEffect(() => {
+    axios({
+      url: EP_GAME,
+      method: 'POST',
+      data: {
+        id: props.match.params.id,
+      },
+    }).then((response) => {
+      setGame(response.data)
+      document.title = `${response.data.name} | Game Hub`
+      console.log(response.data)
+    })
+  }, [props.match.params.id])
+
+  const bgImage =
+    game && game.screenshots
+      ? `https://images.igdb.com/igdb/image/upload/t_1080p/${
+          game.screenshots[Math.floor(Math.random() * game.screenshots.length)]
+            .image_id
+        }.jpg`
+      : ''
+
+  const releaseDate = game
+    ? `${new Date(game.first_release_date * 1000).getDate()} ${new Date(
+        game.first_release_date * 1000
+      ).toLocaleString('default', { month: 'short' })} ${new Date(
+        game.first_release_date * 1000
+      ).getFullYear()}`
+    : ''
+
+  return (
+    <>
+      {!game && <Loader />}
+      {game && (
+        <div
+          className="pt-32 pb-10 md:pt-10 flow-root"
+          style={{
+            backgroundImage: `linear-gradient(rgba(7, 18, 36, 0.5), rgba(31, 41, 55, 1)), url(${bgImage})`,
+            backgroundPosition: `center center`,
+            backgroundSize: `cover`,
+          }}
+        >
+          <Wrapper>
+            {game.cover && (
+              <img
+                src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${game.cover.image_id}.jpg`}
+                alt={game.data}
+                className="rounded-lg shadow-xl mx-auto md:ml-0 md:mr-5 mb-5 mb-0 w-1/2 sm:w-1/3 md:w-auto md:float-left"
+              />
+            )}
+            <Heading1 className="text-center md:text-left">
+              {game.name}
+            </Heading1>
+            <p className="text-sm hidden md:block">
+              <strong>Release date:</strong> {releaseDate}
+            </p>
+            <p className="text-sm hidden md:block">
+              <strong>Platforms:</strong>
+              {` `}
+              {game.platforms.map((platform, index) => (
+                <span key={index}>
+                  {index !== 0 && `, `}
+                  {platform.name}
+                </span>
+              ))}
+            </p>
+          </Wrapper>
+        </div>
+      )}
+    </>
+  )
 }
