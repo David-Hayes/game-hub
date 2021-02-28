4fba422262cb2e9a0bc6f6828759d7c4b1478989
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-02-28 16:12:42 +0000

    Started search page

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,3 +1,46 @@
-export const Search = () => {
-  return <>Search</>
+import { Wrapper } from '../components/Layout'
+
+export const Search = (props) => {
+  return (
+    <Wrapper topSpace>
+      <form>
+        <div className="bg-gray-700 rounded-md flex items-center relative px-3 mx-auto w-min">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-4 w-4 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <input
+            type="text"
+            className="bg-transparent h-10 focus:outline-none"
+          />
+          <button type="reset">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  )
 }
