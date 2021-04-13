cccfd374e55a981ca5ec293edfdd052f223a5bfb
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-04-13 20:03:04 +0100

    Base styling

--- pages/_document.js
+++ pages/_document.js
@@ -9,8 +9,14 @@ class AppDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
-        <body className="bg-gray-800">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="bg-gray-800 text-white">
           <Main />
           <NextScript />
         </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        gray: colors.trueGray,
-      },
+    extend: {},
+    fontFamily: {
+      sans: ['"Open Sans"'],
     },
   },
   variants: {
