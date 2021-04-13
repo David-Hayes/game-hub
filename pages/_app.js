1514c619fb35b8d93b9840e3d3c640e05aa74162
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-04-13 23:25:39 +0100

    Starting to implement user state

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
 import 'tailwindcss/tailwind.css'
+import { AuthProvider } from '../libs/Auth'
 import Header from '../components/Header'
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <AuthProvider>
       <Header />
       <div className="max-w-screen-lg mx-auto px-4">
         <Component {...pageProps} />
       </div>
-    </>
+    </AuthProvider>
   )
 }
 
