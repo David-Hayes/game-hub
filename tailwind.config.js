6c08503c4f57901fb9ffb7a8d4f6a2259aebe8a0
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-04-15 14:29:10 +0100

    Set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        yellow: colors.orange,
+      },
+    },
+    fontFamily: {
+      sans: ['"Open Sans"'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
