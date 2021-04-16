3851911cca718916f53ddc5ef992b2522c2a1e8c
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-04-16 20:36:54 +0100

    Added search page

--- components/Header.js
+++ components/Header.js
@@ -4,26 +4,45 @@ import { useAuth } from '../libs/Auth'
 
 const Header = () => {
   const { user } = useAuth()
-  console.log(user)
   return (
     <header className="bg-gray-900 text-white">
       <Container className="flex items-center justify-between py-3">
         <Link href="/">
-          <a className="font-semibold text-2xl">
+          <a className="font-semibold text-xl">
             game<span className="text-yellow-500">hub</span>
           </a>
         </Link>
-        {user ? (
-          <Link href="/profile">
-            <a>
-              <img
-                src={user.photoUrl}
-                alt={user.name}
-                className="rounded-full w-10"
-              />
+        <div className="flex items-center">
+          <Link href="/search">
+            <a title="Search">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
             </a>
           </Link>
-        ) : null}
+          {user ? (
+            <Link href="/profile">
+              <a title="Profile">
+                <img
+                  src={user.photoUrl}
+                  alt={user.name}
+                  className="rounded-full w-7 ml-4"
+                />
+              </a>
+            </Link>
+          ) : null}
+        </div>
       </Container>
     </header>
   )
---
+++ pages/search.js
@@ -0,0 +1,7 @@
+import Wrapper from '../components/Wrapper'
+
+const Search = () => {
+  return <Wrapper></Wrapper>
+}
+
+export default Search
