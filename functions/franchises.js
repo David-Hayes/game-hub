1c74a72f0bd256b62678380d2773dd12bb675b90
David Hayes <19607539+David-Hayes@users.noreply.github.com>
2021-04-30 21:57:51 +0100

    Basic franchises

---
+++ functions/franchises.js
@@ -0,0 +1,79 @@
+const { default: axios } = require('axios')
+const { IGDB_CLIENT } = process.env
+
+exports.handler = async (event, context) => {
+  // check for get method
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ error: 'Method not accepted' }),
+    }
+  }
+
+  // do we have a bearer token?
+  if (
+    !event.headers.cookie ||
+    event.headers.cookie.match(new RegExp('(^| )gDBT=([^;]+)')) === null
+  ) {
+    return {
+      statusCode: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        msg: 'No bearer token',
+      }),
+    }
+  }
+
+  // do we have a query?
+  if (!event.body && !event.body.query) {
+    return {
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        msg: 'No query',
+      }),
+    }
+  }
+
+  const { id: id } = JSON.parse(event.body)
+
+  // set our headers for igdb
+  const defaultHeaders = {
+    Accept: 'application/json',
+    'Client-ID': IGDB_CLIENT,
+    Authorization: `Bearer ${
+      event.headers.cookie.match(new RegExp('(^| )gDBT=([^;]+)'))[2]
+    }`,
+  }
+
+  return axios({
+    url: 'https://api.igdb.com/v4/franchises',
+    method: 'POST',
+    headers: defaultHeaders,
+    data: `fields name, games.name, games.slug, games.cover.*, games.first_release_date; where slug = "${id}";`,
+  })
+    .then((response) => ({
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-count': response.headers['x-count'],
+      },
+      body: JSON.stringify(response.data),
+    }))
+    .catch((err) => ({
+      statusCode: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        error: err,
+      }),
+    }))
+}
